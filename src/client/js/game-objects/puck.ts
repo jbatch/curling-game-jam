2410094a08dbe0d33c856037e1ac704ede469b43
Josh Batchelor <josh_batch@live.com.au>
2019-11-09 16:48:01 +1100

    Refactor all the launch code into puck.ts

---
+++ src/client/js/game-objects/puck.ts
@@ -0,0 +1,86 @@
+'use strict';
+
+import 'phaser';
+
+type Props = {
+  scene: Phaser.Scene;
+  x: number;
+  y: number;
+  texture: string;
+  frame?: string | integer;
+};
+
+export default class Puck extends Phaser.GameObjects.Image {
+  public body!: Phaser.Physics.Arcade.Body;
+  private trajectoryLine: Phaser.GameObjects.Line;
+  private startX: number;
+  private startY: number;
+
+  constructor({ scene, x, y, texture, frame }: Props) {
+    super(scene, x, y, texture, frame);
+    this.setDisplayOrigin(0.5);
+    this.setOrigin(0.5);
+    this.scene.physics.world.enable(this);
+    this.body.setAllowGravity(false);
+    this.body.setAllowDrag(true);
+    this.body.setBounce(1, 1);
+    this.body.setCollideWorldBounds(true);
+    this.scene.add.existing(this);
+
+    this.scene.input.on('pointerdown', this.startLauch, this);
+  }
+
+  startLauch(e: Phaser.Input.Pointer) {
+    this.startX = e.x;
+    this.startY = e.y;
+    this.trajectoryLine = this.scene.add
+      .line(
+        0,
+        0,
+        this.x,
+        this.y,
+        this.x + (e.x - this.startX),
+        this.y + (e.y - this.startY),
+        0xff0000,
+        1.0
+      )
+      .setOrigin(0, 0);
+
+    this.scene.input.removeListener('pointerdown', this.startLauch, this);
+    this.scene.input.addListener('pointermove', this.chargeLaunch, this);
+    this.scene.input.on('pointerup', this.releaseLaunch, this);
+  }
+
+  chargeLaunch(e) {
+    this.trajectoryLine.destroy();
+    this.trajectoryLine = this.scene.add
+      .line(
+        0,
+        0,
+        this.x,
+        this.y,
+        this.x + (e.x - this.startX),
+        this.y + (e.y - this.startY),
+        0xff0000,
+        1.0
+      )
+      .setOrigin(0, 0);
+  }
+
+  releaseLaunch(e) {
+    this.trajectoryLine.destroy();
+    this.scene.input.removeListener('pointermove', this.chargeLaunch);
+    this.scene.input.removeListener('pointerup', this.releaseLaunch);
+    this.scene.input.on('pointerdown', this.startLauch, this);
+    const lineLength = Phaser.Math.Distance.Between(e.x, e.y, this.startX, this.startY);
+    const rotation = Phaser.Math.Angle.BetweenPoints(
+      { x: e.x, y: e.y },
+      { x: this.startX, y: this.startY }
+    );
+    this.scene.physics.velocityFromRotation(
+      rotation,
+      lineLength * 2,
+      (this.body as Phaser.Physics.Arcade.Body).velocity
+    );
+  }
+}
--- src/client/js/scenes/game-scene.ts
+++ src/client/js/scenes/game-scene.ts
@@ -1,13 +1,12 @@
 'use strict';
 
 import 'phaser';
+import Puck from '../game-objects/puck';
 
 export default class GameScene extends Phaser.Scene {
-  private trajectoryGraphics;
+  
   private puck: Phaser.GameObjects.Image;
-  private trajectoryLine: Phaser.GameObjects.Line;
-  private startX;
-  private startY;
+  
 
   constructor() {
     super('GameScene');
@@ -20,71 +19,10 @@ export default class GameScene extends Phaser.Scene {
   }
 
   create() {
-    this.puck = this.add.image(500, 550, 'puck');
-    this.puck.setDisplayOrigin(0.5);
-    this.puck.setOrigin(0.5);
-    this.physics.add.existing(this.puck);
-    const body = this.puck.body as Phaser.Physics.Arcade.Body;
-    body.setAllowGravity(false);
-    body.setAllowDrag(true);
-    body.setBounce(1,1);
-    body.setCollideWorldBounds(true);
-    this.input.on('pointerdown', this.startLauch, this);
+    this.puck = new Puck({scene: this, x: 500, y: 550, texture: 'puck'});
   }
 
-  startLauch(e: Phaser.Input.Pointer) {
-    this.startX = e.x;
-    this.startY = e.y;
-    this.trajectoryLine = this.add
-      .line(
-        0,
-        0,
-        this.puck.x,
-        this.puck.y,
-        this.puck.x + (e.x - this.startX),
-        this.puck.y + (e.y - this.startY),
-        0xff0000,
-        1.0
-      )
-      .setOrigin(0, 0);
-
-    this.input.removeListener('pointerdown', this.startLauch, this);
-    this.input.addListener('pointermove', this.chargeLaunch, this);
-    this.input.on('pointerup', this.releaseLaunch, this);
-  }
-
-  chargeLaunch(e) {
-    this.trajectoryLine.destroy();
-    this.trajectoryLine = this.add
-      .line(
-        0,
-        0,
-        this.puck.x,
-        this.puck.y,
-        this.puck.x + (e.x - this.startX),
-        this.puck.y + (e.y - this.startY),
-        0xff0000,
-        1.0
-      )
-      .setOrigin(0, 0);
-  }
-
-  releaseLaunch(e) {
-    this.trajectoryLine.destroy();
-    this.input.removeListener('pointermove', this.chargeLaunch);
-    this.input.removeListener('pointerup', this.releaseLaunch);
-    this.input.on('pointerdown', this.startLauch, this);
-    const lineLength = Phaser.Math.Distance.Between(e.x, e.y, this.startX, this.startY);
-    const rotation = Phaser.Math.Angle.BetweenPoints(
-      { x: e.x, y: e.y },
-      { x: this.startX, y: this.startY },
-    );
-    this.physics.velocityFromRotation(
-      rotation,
-      lineLength * 2,
-      (this.puck.body as Phaser.Physics.Arcade.Body).velocity
-    );
-  }
+  
 
   update(time, delta) {
     (this.puck.body as Phaser.Physics.Arcade.Body).velocity.multiply(new Phaser.Math.Vector2({x: 0.99, y: 0.99}));
